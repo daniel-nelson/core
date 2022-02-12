a0a35bce048a85f23200e794b1068e1e6b6fca3c
Daniel <daniel@platejoy.com>
2022-02-12 10:54:26 -0600

    Revert "Include build folder"
    This reverts commit 267e8ecd426a7301481dd48a0d4f2302b9f3eb50.

--- build/instructions.d.ts
+++
@@ -1,6 +0,0 @@
-import * as sinkStatic from '@adonisjs/sink';
-import { ApplicationContract } from '@ioc:Adonis/Core/Application';
-/**
- * Configure package
- */
-export default function instructions(projectRoot: string, _: ApplicationContract, { logger, files }: typeof sinkStatic): Promise<void>;
--- build/test/serve-static.spec.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="../adonis-typings/index.d.ts" />
-export {};
